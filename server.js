6b3293ef50ad4deece5ebb560ebb2dbb3a985aeb
Harun Aliyev <AliyevHrn@yandex.ru>
2021-07-18 15:22:14 +0300

    add express js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(__dirname + '/dist'));
+console.log(__dirname);
+
+
+app.listen(PORT, () => {
+    console.log(`Запускаем наше приложение на порту ${PORT} ${__dirname + '/dist'}`);
+});
